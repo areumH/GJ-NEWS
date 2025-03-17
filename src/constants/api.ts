944d47ffd965c93bc35e277ad286d6b47f3344ec
areumH <94692566+areumH@users.noreply.github.com>
2025-03-17 22:33:19 +0900

    feat: http 코드 및 엔드포인트 작성

---
+++ src/constants/api.ts
@@ -0,0 +1,11 @@
+export const HTTP_STATUS_CODE = {
+  OK: 200,
+  BAD_REQUEST: 400,
+  NOT_FOUND: 404,
+  SYSTEM_ERROR: 500,
+} as const;
+
+export const END_POINT = {
+  NEWS_SEARCH: (encodedQuery: string, display: number, start: number, sort: string) =>
+    `/search/news.json?query=${encodedQuery}&display=${display}&start=${start}&sort=${sort}`,
+};
