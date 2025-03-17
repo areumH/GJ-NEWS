ed940de20516042d583d2dafe7fa127cea1598d7
areumH <94692566+areumH@users.noreply.github.com>
2025-03-17 21:20:19 +0900

    feat: 검색 파라미터 타입 작성

---
+++ src/types/search.ts
@@ -0,0 +1,11 @@
+export interface NewsSearchParams {
+  query: string;
+  // 검색 키워드
+  display: number;
+  // 한 번에 표시할 뉴스 항목 수 (기본값: 10, 최댓값: 100)
+  start: number;
+  // 검색 결과 시작 위치 (기본값: 1, 최댓값: 1000)
+  sort: 'sim' | 'date';
+  // 'sim' - 정확도 순 정렬
+  // 'date' - 날짜 순 정렬
+}
