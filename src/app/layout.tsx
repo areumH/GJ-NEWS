5d0ab5ff0c9f7b27cbc8c02eae6ea844967383f4
areumH <94692566+areumH@users.noreply.github.com>
2025-04-01 18:56:50 +0900

    feat: 페이지 전체에 Pretendard 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { pretendard } from '@/styles/font';
 import '@/styles/globals.css';
 
 export const metadata: Metadata = {
@@ -13,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={pretendard.className}>{children}</body>
     </html>
   );
 }
