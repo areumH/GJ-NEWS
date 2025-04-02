49b3e908393099729074a8aaa72775438c3de8ad
areumH <94692566+areumH@users.noreply.github.com>
2025-04-03 01:41:04 +0900

    feat: 페이지 기본 가로 길이 지정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={pretendard.className}>{children}</body>
+      <body className={`${pretendard.className} max-w-[1200px] mx-auto`}>{children}</body>
     </html>
   );
 }
