efb8f4d96d1756c6981c589c75a15ca0c120cebf
areumH <94692566+areumH@users.noreply.github.com>
2025-04-03 18:27:56 +0900

    feat: 검색 페이지 좌우 패딩 추가

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -10,9 +10,9 @@ export default function Search() {
   const page = Number(searchParams.get('page')) || 1;
 
   return (
-    <div className="flex flex-col w-full min-h-screen items-center py-6 bg-gray-50">
+    <div className="flex flex-col w-full min-h-screen items-center px-7 sm:px-9 py-6 bg-gray-50">
       <SearchBar />
-      <div className='flex flex-col w-full mt-7 sm:mt-9 gap-2 sm:gap-5'>
+      <div className="flex flex-col w-full mt-7 sm:mt-9 gap-2 sm:gap-5">
         <NewsCard />
         <NewsCard />
       </div>
