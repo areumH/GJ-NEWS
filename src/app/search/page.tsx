0c62b58ea3572ceaf9793b2d23978cef94f0b10a
areumH <94692566+areumH@users.noreply.github.com>
2025-04-03 21:29:52 +0900

    feat: 뉴스 카드 컴포넌트 스타일 수정 및 제목만 보여질 수 있도록 필터링 추가

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -13,8 +13,8 @@ export default function Search() {
     <div className="flex flex-col w-full min-h-screen items-center px-7 sm:px-9 py-6 bg-gray-50">
       <SearchBar />
       <div className="flex flex-col w-full mt-7 sm:mt-9 gap-2 sm:gap-5">
-        <NewsCard />
-        <NewsCard />
+        <NewsCard isTitleOnly={true} />
+        <NewsCard isTitleOnly={false} />
       </div>
       페이지: {page}
     </div>
--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -2,16 +2,21 @@ import { NewsItem } from '@/types/news';
 
 export interface NewsCardProps {
   news: NewsItem;
+  isTitleOnly: boolean;
 }
 
 // title description originallink link? pubDate
 
-const NewsCard = () => {
+const NewsCard = ({ isTitleOnly }: { isTitleOnly: boolean }) => {
   return (
-    <button className="flex flex-col w-full p-5 bg-white border-2 border-gray-200">
-      <div>2025.02.03</div>
-      <div>기사 제목</div>
-      <div>기사 내용</div>
+    <button className="flex flex-col w-full p-5 sm:p-7 gap-1 text-left bg-white rounded-lg outline-1 sm:hover:outline-3 outline-gray-200  hover:outline-indigo-100 cursor-pointer">
+      <div className="w-full text-sm sm:text-lg text-gray-400">2025.02.03</div>
+      <div className="w-full text-lg sm:text-2xl text-indigo-800 font-semibold line-clamp-2 leading-snug">
+        기사 제목
+      </div>
+      {isTitleOnly && (
+        <div className="w-full sm:text-lg text-gray-700 line-clamp-3 leading-snug">기사 내용</div>
+      )}
     </button>
   );
 };
