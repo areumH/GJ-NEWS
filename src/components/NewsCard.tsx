61bd3147f12a7dac060df89c6728d7a2a0ce2c9a
areumH <94692566+areumH@users.noreply.github.com>
2025-04-14 21:45:20 +0900

    feat: 날짜 문자열 포맷 함수 작성 및 적용

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -1,4 +1,5 @@
 import { NewsItem } from '@/types/news';
+import { formatDate } from '@/utils/date';
 
 export interface NewsCardProps {
   news: NewsItem;
@@ -15,7 +16,7 @@ const NewsCard = ({ news, isTitleOnly }: NewsCardProps) => {
       onClick={handleNewsCard}
       className="flex flex-col w-full p-5 sm:p-7 gap-1 text-left bg-white rounded-lg outline-1 sm:hover:outline-3 outline-gray-200  hover:outline-indigo-100 cursor-pointer"
     >
-      <div className="w-full text-sm sm:text-lg text-gray-400">{news.pubDate}</div>
+      <div className="w-full text-sm sm:text-lg text-gray-400">{formatDate(news.pubDate)}</div>
       <div className="w-full text-lg sm:text-2xl text-indigo-800 font-semibold line-clamp-2 leading-snug">
         {news.title}
       </div>
