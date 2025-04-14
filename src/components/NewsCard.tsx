bfe617774019667efc8c49d1d0c7904a13f0f901
areumH <94692566+areumH@users.noreply.github.com>
2025-04-14 22:27:38 +0900

    feat: 데이터의 b 태그 스타일이 적용되도록 뉴스 카드 수정

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -17,13 +17,15 @@ const NewsCard = ({ news, isTitleOnly }: NewsCardProps) => {
       className="flex flex-col w-full p-5 sm:p-7 gap-1 text-left bg-white rounded-lg outline-1 sm:hover:outline-3 outline-gray-200  hover:outline-indigo-100 cursor-pointer"
     >
       <div className="w-full text-sm sm:text-lg text-gray-400">{formatDate(news.pubDate)}</div>
-      <div className="w-full text-lg sm:text-2xl text-indigo-800 font-semibold line-clamp-2 leading-snug">
-        {news.title}
-      </div>
+      <div
+        className="w-full text-lg sm:text-2xl text-indigo-800 font-semibold line-clamp-2 leading-snug"
+        dangerouslySetInnerHTML={{ __html: news.title }}
+      ></div>
       {!isTitleOnly && (
-        <div className="w-full sm:text-lg text-gray-700 line-clamp-3 leading-snug">
-          {news.description}
-        </div>
+        <div
+          className="w-full sm:text-lg text-gray-700 line-clamp-3 leading-snug"
+          dangerouslySetInnerHTML={{ __html: news.description }}
+        ></div>
       )}
     </button>
   );
