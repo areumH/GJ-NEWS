a08d4cf995394e11612d49bf087945c2ff8b0ca6
areumH <94692566+areumH@users.noreply.github.com>
2025-04-26 00:27:38 +0900

    refactor: 검색 결과 메세지 컴포넌트 스타일 수정

--- src/components/NoResultMessage.tsx
+++ src/components/NoResultMessage.tsx
@@ -4,11 +4,11 @@ export interface NoResultMessageProps {
 
 const NoResultMessage = ({ keyword }: NoResultMessageProps) => {
   return (
-    <div className="flex flex-col items-center gap-0.5 sm:text-xl">
-      <p>
+    <div className="flex flex-col w-full items-center gap-0.5 sm:text-xl">
+      <div className="text-center break-words">
         <span className="text-indigo-800">&apos;{keyword}&apos;</span>에 대한 검색 결과가 없습니다.
-      </p>
-      <p>검색어를 다시 확인해주세요!</p>
+      </div>
+      <div className="text-center break-words">검색어를 다시 확인해주세요!</div>
     </div>
   );
 };
