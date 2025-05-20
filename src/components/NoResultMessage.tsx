e80f9b64ea57a20138d2b456e3887f56672ddaec
areumH <94692566+areumH@users.noreply.github.com>
2025-05-20 23:46:34 +0900

    feat: 검색 결과가 없을 때의 텍스트를 상수로 분리

--- src/components/NoResultMessage.tsx
+++ src/components/NoResultMessage.tsx
@@ -1,3 +1,5 @@
+import { SEARCH_RESULT } from '@/constants/messages';
+
 export interface NoResultMessageProps {
   keyword: string;
 }
@@ -6,9 +8,10 @@ const NoResultMessage = ({ keyword }: NoResultMessageProps) => {
   return (
     <div className="flex flex-col w-full items-center gap-0.5 sm:text-xl">
       <div className="text-center break-words">
-        <span className="text-indigo-800">&apos;{keyword}&apos;</span>에 대한 검색 결과가 없습니다.
+        <span className="text-indigo-800">&apos;{keyword}&apos;</span>
+        {SEARCH_RESULT.NONE}
       </div>
-      <div className="text-center break-words">검색어를 다시 확인해주세요!</div>
+      <div className="text-center break-words">{SEARCH_RESULT.CHECK_KEYWORD}</div>
     </div>
   );
 };
--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -10,3 +10,8 @@ export const IMAGE_ALTS = {
   LOADING: '로딩 중',
   NEGATIVE: '부정 뉴스',
 };
+
+export const SEARCH_RESULT = {
+  NONE: '에 대한 검색 결과가 없습니다.',
+  CHECK_KEYWORD: '검색어를 다시 확인해주세요!',
+};
