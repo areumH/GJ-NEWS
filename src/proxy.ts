f1cb147f4abdb7cc98c8e6b9d9b20c3cf4cf0f64
areumH <dkfmal0109@gmail.com>
2026-03-19 20:12:18 +0900

    chore: middleware 파일명을 proxy로 변경

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   const { searchParams } = req.nextUrl;
 
   if (!searchParams.get('query')?.trim()) {
