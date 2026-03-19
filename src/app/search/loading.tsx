2391899dce3ef8f40c40d7885d476c6027aaac79
areumH <dkfmal0109@gmail.com>
2026-03-20 02:30:33 +0900

    feat: FilterState 오류 수정

--- src/app/search/loading.tsx
+++ src/app/search/loading.tsx
@@ -5,7 +5,6 @@ import FilterOption from '@/components/FilterOption';
 
 export default function Search() {
   const filter: FilterState = {
-    sort: 'sim',
     showPositiveOnly: false,
     showTitleOnly: false,
   };
@@ -13,7 +12,7 @@ export default function Search() {
   return (
     <div className="flex flex-col w-full min-h-screen items-center px-7 sm:px-12 py-6 gap-5 sm:gap-6 bg-gray-50">
       <SearchBar keyword={''} />
-      <FilterOption filter={filter} />
+      <FilterOption sort="sim" filter={filter} />
       <div className="flex w-full justify-center items-center mt-50 sm:mt-40">
         <SpinnerIcon className="w-10 h-10 text-indigo-800 animate-spin" style={{ animationDuration: '1.5s' }} />
       </div>
